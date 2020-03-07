cff2a9bfcb27a4d6cad1c1563c5528125f0b9cbf
the-c0d3r <the-c0d3r@users.noreply.github.com>
2020-03-07 15:07:58 +0630

    lib/entry.py: renamed all meaning to definition

--- lib/entry.py
+++ lib/entry.py
@@ -7,23 +7,23 @@ from typing import Dict
 class Entry:
     """Dictionary Entry object"""
 
-    def __init__(self, word: str, meaning: str):
+    def __init__(self, word: str, definition: str):
         """
         Initialize the entry object
         :param word: the word to store
-        :param meaning: Meaning of the word
+        :param definition: definition of the word
         """
         self._word = word
-        self._meaning = meaning
+        self._definition = definition
 
     def get_data(self) -> Dict:
         """Function to convert it into data format for db insertion"""
         return {
             'word': self._word,
-            'definition': self._meaning
+            'definition': self._definition
         }
 
     def str(self) -> str:
         """return str"""
-        return f"{self._word}: {self._meaning}"
+        return f"{self._word}: {self._definition}"
 
