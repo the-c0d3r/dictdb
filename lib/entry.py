74f6531b3343180720ffeeca79ee69b3644c3642
the-c0d3r <the-c0d3r@users.noreply.github.com>
2020-05-24 08:44:40 +0800

    lib/entry.py: modified the pattern to support phrase, added properties, updated unit test

--- lib/entry.py
+++ lib/entry.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import re
 from typing import Dict
 
 
 class Entry:
     """Dictionary Entry object"""
-    pattern = re.compile(r"(\S+):\s?(.+)")
+    pattern = re.compile(r"(.+)\s?:\s?(.+)")
 
     def __init__(self, data: str) -> None:
         """
@@ -14,21 +12,38 @@ class Entry:
         :param data: string in the following format
         word : meaning
         """
-        [[word, definition]] = self.pattern.findall(data)
-        self._word = word.strip()
-        self._definition = definition.strip()
+        try:
+            [[word, definition]] = self.pattern.findall(data)
+        except ValueError:
+            # means parsing failed
+            self._word = ""
+            self._definition = ""
+        else:
+            self._word = word.strip()
+            self._definition = definition.strip()
 
     def is_valid(self) -> bool:
         return self._word != "" and self._definition != ""
 
-    def get_data(self) -> Dict:
-        """Function to convert it into data format for db insertion"""
+    @property
+    def word(self) -> str:
+        return self._word
+
+    @property
+    def definition(self) -> str:
+        return self._definition
+
+    def get_dict(self) -> Dict:
+        """convert entry into dict data format for db insertion"""
         return {
-            'word': self._word,
+            'word'      : self._word,
             'definition': self._definition
         }
+
+    def get_str(self) -> str:
+        """convert the entry into printable readable string"""
+        return self.str()
 
     def str(self) -> str:
         """return str"""
         return f"{self._word}: {self._definition}"
-
--- tests/test_entry.py
+++ tests/test_entry.py
@@ -9,11 +9,41 @@ def entry():
 
 
 class TestEntry:
-    def test_init(self, entry):
-        assert entry is not None
+    def test_init(self):
+        entry = Entry("test : abcdef")
+        assert entry.is_valid()
+
+        entry = Entry("test:abcdef")
+        assert entry.is_valid()
+        assert entry.word == "test"
+        assert entry.definition == "abcdef"
+
+        entry = Entry("test :abcdef")
+        assert entry.is_valid()
+        assert entry.word == "test"
+        assert entry.definition == "abcdef"
+
+        entry = Entry("test: abcdef")
+        assert entry.is_valid()
+        assert entry.word == "test"
+        assert entry.definition == "abcdef"
+
+        entry = Entry("test")
+        assert not entry.is_valid()
+
+    def test_phrase(self):
+        entry = Entry("zero sum: 0")
+        assert entry.is_valid()
+
+        assert entry.word == "zero sum"
+        assert entry.definition == "0"
+
+    def test_props(self, entry):
+        assert entry.word == "test"
+        assert entry.definition == "word definition"
 
     def test_get_data(self, entry):
-        data = entry.get_data()
+        data = entry.get_dict()
         assert data.get("word") == "test"
         assert data.get("definition") == "word definition"
 
